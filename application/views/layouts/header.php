dbbc4997c9d6a2e238a798f3b46b60b2a2ee9662
agungsenjaya <agungsenjaya813@gmail.com>
2019-08-12 13:55:50 +0700

    'to'

---
+++ application/views/layouts/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Admin Dashboard</title>
+	<link rel="stylesheet" href="">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/app.css">
+</head>
+<body>
+	<div class="row">
+		
+	</div>
---
+++ application/views/v_login.php
@@ -0,0 +1,25 @@
+<div class="d-flex justify-content-center align-items-center min-vh-100 py-6">
+          <div class="">
+            <div class="wd-100p">
+              <!-- <h3 class="tx-color-01 mg-b-5">Sign In</h3> -->
+              <div class="text-center">
+                <h1><i class="fa fa-server mr-2"></i>Desty project solution</h1>
+               <div class="tx-color-02 tx-16 mg-b-20">Selamat datang, silahkan login terlebih dahulu.</div>
+              </div>
+
+              <div class="form-group">
+                <label>Username</label>
+                <input type="text" class="form-control" placeholder="Masukan username">
+              </div>
+              <div class="form-group">
+                <div class="d-flex justify-content-between mg-b-5">
+                  <label class="mg-b-0-f">Password</label>
+                  <!-- <small>Masukan username dan password yang benar</small> -->
+                </div>
+                <input type="password" class="form-control" placeholder="Masukan password">
+              </div>
+              <button class="btn btn-brand-02 btn-block">Sign In</button>
+              <div class="divider-text">Desty Project <?php echo date('Y'); ?></div>
+            </div>
+          </div>
+        </div>
